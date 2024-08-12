07438eacca2802d0332e7a4ee8517246c459df8e
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-08-12 16:53:17 -0300

    feat: apply the css file to the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 
+import './index.css';
+
 const container = document.getElementById('root');
 
 createRoot(container!).render(<App />);
