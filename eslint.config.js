7e0e153ffcdd30a99ac504d5807bb05c1deb1862
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-08-12 16:23:42 -0300

    feat: add .editorconfig file

--- eslint.config.js
+++ eslint.config.js
@@ -5,24 +5,24 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{js,jsx,ts,tsx}'],
-  ignores: ['dist'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser
-  },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true }
-    ],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always']
-  }
+	extends: [js.configs.recommended, ...tseslint.configs.recommended],
+	files: ['**/*.{js,jsx,ts,tsx}'],
+	ignores: ['dist'],
+	languageOptions: {
+		ecmaVersion: 2020,
+		globals: globals.browser
+	},
+	plugins: {
+		'react-hooks': reactHooks,
+		'react-refresh': reactRefresh
+	},
+	rules: {
+		...reactHooks.configs.recommended.rules,
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true }
+		],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always']
+	}
 });
