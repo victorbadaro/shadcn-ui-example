3a272a6cff45418325fa377c625dca8746a1134e
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-08-12 17:23:00 -0300

    feat: add a table to show products

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table';
+
 export function App() {
-	return <h1>Hello, world!</h1>;
+	return (
+		<div className="p-6 max-w-4xl mx-auto">
+			<div className="border rounded-lg p-2">
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead>ID</TableHead>
+							<TableHead>Produto</TableHead>
+							<TableHead>Preço</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{Array.from({ length: 10 }).map((_, index) => (
+							<TableRow key={index}>
+								<TableCell>{crypto.randomUUID()}</TableCell>
+								<TableCell>Produto {(index + 1).toString().padStart(2, '0')}</TableCell>
+								<TableCell>R$ 192,00</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</div>
+		</div>
+	);
 }
